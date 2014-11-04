e0805aabd1a17059a3aaafc614816829c8a88d82
Jorja Hung <jorjahung@gmail.com>
2014-11-04 15:47:14 +0000

    refactoring

--- app/models/checker.rb
+++ app/models/checker.rb
@@ -7,41 +7,53 @@ class Checker
 
   def port_open?(ip, port, seconds=1)
     Timeout::timeout(seconds) do
-      begin
-        TCPSocket.new(ip, port).close
-        true
-      rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH => error_message
-        @errors << error_message.message
-        false
-      end
+      connect_to_socket(ip,port)
     end
-  rescue Timeout::Error, SocketError => error_message
-    @errors << error_message.message
-    false
+    rescue Timeout::Error, SocketError => error
+      log_error(error)
+      false
   end
 
   def url_exists?(url_string, seconds=5)
     Timeout::timeout(seconds) do
-      begin
-        url_string = url_validator(url_string)
-        url = URI.parse(url_string)
-        request = Net::HTTP.new(url.host, url.port)
-        request.use_ssl = (url.scheme == 'https')
-        path = !url.path.empty? ? url.path : '/'
-        response = request.request_head(path)
-        if response.kind_of?(Net::HTTPRedirection)
-          url_exists?(response['location']) # Go after any redirect and make sure you can access the redirected URL
-        else
-          ! %W(4 5).include?(response.code[0]) # Not from 4xx or 5xx families
-        end
-      rescue => error_message
-        @errors << error_message.message
-        false #false if can't find the server
-      end
+      connect_to_url(url_string)
     end
-    rescue Timeout::Error, SocketError => error_message
-      @errors << error_message.message
+    rescue Timeout::Error, SocketError => error
+      log_error(error)
       false
+  end
+
+  def connect_to_url(url_string)
+    begin
+      url_string = url_validator(url_string)
+      url = URI.parse(url_string)
+      request = Net::HTTP.new(url.host, url.port)
+      request.use_ssl = (url.scheme == 'https')
+      path = !url.path.empty? ? url.path : '/'
+      response = request.request_head(path)
+      if response.kind_of?(Net::HTTPRedirection)
+        url_exists?(response['location']) # Go after any redirect and make sure you can access the redirected URL
+      else
+        ! %W(4 5).include?(response.code[0]) # Not from 4xx or 5xx families
+      end
+    rescue => error
+      log_error(error)
+      false #false if can't find the server
+    end
+  end
+
+  def connect_to_socket(ip,port)
+    begin
+      TCPSocket.new(ip, port).close
+      true
+    rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH => error
+      log_error(error)
+      false
+    end
+  end
+
+  def log_error(error)
+    @errors << error.message
   end
 
   def url_validator(url_string)
--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -40,8 +40,13 @@ describe Checker do
   end
 
   context "error log" do
-    it "knows about errors" do
-      check = @checker.url_exists?("http://www.thisdomaindoesntexist.com/not/even/here")
+    it "knows about errors in urls" do
+      @checker.url_exists?("http://www.thisdomaindoesntexist.com/not/even/here")
+      expect(@checker.errors).to_not be_empty
+    end
+
+    it "knows about errors in tcp" do
+      @checker.port_open?("www.thisdomaindoesntexist.com", 80)
       expect(@checker.errors).to_not be_empty
     end
   end
