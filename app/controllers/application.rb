9db54a363c7d3c1971ad58e9703aceb3043610b4
Jorja Hung <jorjahung@gmail.com>
2014-11-05 16:44:48 +0000

    Trying to make it work on CF

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,3 +1,4 @@
+
 get '/' do
   erb :index
 end
@@ -5,12 +6,37 @@ end
 post '/tcp' do
   host = params['host']
   port = params['port']
-  redirect to("/check?host=#{host}&port=#{port}")
+  redirect to("/tcp/#{host}/#{port}")
 end
 
 post '/url' do
   url = params['url']
-  redirect to("/check?url=#{url}")
+  redirect to("/url/#{url}")
+end
+
+get '/tcp/:host/:port' do
+  host = params['host']
+  port = params['port']
+  check = Checker.new
+  if check.port_open?(host, port)
+    flash[:true] = "Yes! I can reach #{host}:#{port}!"
+  else
+    flash[:false] = "Boohoo... I can't reach #{host}:#{port}."
+    flash[:errors] = "This is what happened:<br>#{check.errors.map(&:capitalize).join("<br>")}"
+  end
+  erb :index
+end
+
+get '/url/:url' do
+  url = params['url']
+  check = Checker.new
+  if check.url_exists?(url)
+    flash[:true] = "Yes! I can reach #{url}!"
+  else
+    flash[:false] = "Boohoo... I can't reach #{url}."
+    flash[:errors] = "This is what happened:<br>#{check.errors.map(&:capitalize).join("<br>")}"
+  end
+  erb :index
 end
 
 get '/check' do
