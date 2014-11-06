40ec9af835230c71436f6d9aab2ce8d064eb25a1
Jorja Hung <jorjahung@gmail.com>
2014-11-06 10:55:57 +0000

    Change to query params

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,40 +6,42 @@ end
 post '/tcp' do
   host = params['host']
   port = params['port']
-  redirect to("/tcp/#{host}/#{port}")
+  redirect to("/check?host=#{host}&port=#{port}")
 end
 
 post '/url' do
-  url = CGI.escape(params['url'])
-  redirect to("/url/#{url}")
+  # url = CGI.escape(params['url'])
+  url = params['url']
+
+  redirect to("/check?url=#{url}")
 end
 
-get '/tcp/:host/:port' do
-  host = params['host']
-  port = params['port']
-  check = Checker.new
-  if check.port_open?(host, port)
-    @true = "Yes! I can reach #{host}:#{port}!"
-  else
-    @false = "Boohoo... I can't reach #{host}:#{port}."
-    @errors = "This is what happened:<br>#{check.errors.map(&:capitalize).join("<br>")}"
-  end
-  erb :index
-end
+# get '/tcp/:host/:port' do
+#   host = params['host']
+#   port = params['port']
+#   check = Checker.new
+#   if check.port_open?(host, port)
+#     @true = "Yes! I can reach #{host}:#{port}!"
+#   else
+#     @false = "Boohoo... I can't reach #{host}:#{port}."
+#     @errors = "This is what happened:<br>#{check.errors.map(&:capitalize).join("<br>")}"
+#   end
+#   erb :index
+# end
 
-get '/url/*' do
-  address = params[:splat].first
+# get '/url/*' do
+#   address = params[:splat].first
 
-  url = address.sub( %r{^http(s)?:\/?}, 'http\1://' )
-  check = Checker.new
-  if check.url_exists?(url)
-    @true = "Yes! I can reach #{url}!"
-  else
-    @false = "Boohoo... I can't reach #{url}."
-    @errors = "This is what happened:<br>#{check.errors.map(&:capitalize).join("<br>")}"
-  end
-  erb :index
-end
+#   url = address.sub( %r{^http(s)?:\/?}, 'http\1://' )
+#   check = Checker.new
+#   if check.url_exists?(url)
+#     @true = "Yes! I can reach #{url}!"
+#   else
+#     @false = "Boohoo... I can't reach #{url}."
+#     @errors = "This is what happened:<br>#{check.errors.map(&:capitalize).join("<br>")}"
+#   end
+#   erb :index
+# end
 
 get '/check' do
   url = params[:url]
